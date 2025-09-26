4f2b56f45c1d1364b4664015c2ec11bc8f5b2a81
Oskari Klaas <Oskariklaas06@gmail.com>
2025-09-26 13:46:20 +0300

    Ülesanne: Matemaatilised avaldised

---
+++ math_exercises.py
@@ -0,0 +1,12 @@
+#1. ulesanne sum and difference
+num_a = 5
+num_b = 2
+sum = num_a + num_b
+difference = num_a - num_b
+
+# float division
+division = num_a / num_b
+
+# integer division
+
+division = num_a // num_b
