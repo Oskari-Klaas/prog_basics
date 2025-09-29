0441df23e9a945d90c5bdf7af70c0b280a4e4ea7
Oskari Klaas <Oskariklaas06@gmail.com>
2025-09-29 10:52:09 +0300

    Ülesanne: Matemaatilised avaldised

--- math_exercises.py
+++ math_exercises.py
@@ -1,3 +1,4 @@
+import math
 #1. ulesanne sum and difference
 num_a = 5
 num_b = 2
@@ -10,3 +11,36 @@ division = num_a / num_b
 # integer division
 
 division = num_a // num_b
+
+# Powerful operations
+
+multiply_numbers = num_a * num_b
+power = num_a ** num_b
+remainder = num_a % num_b
+
+# find average
+
+average = (num_a + num_b) / 2
+
+# area of a circle
+
+radius = 5
+circle_area = math.pi * radius ** 2
+circle_area = round(circle_area, 2)
+
+# area of an equilateral triangle
+side_length = 3
+triangle_area = math.sqrt(3) / 4 * side_length ** 2
+triangle_area = round(triangle_area)
+
+#8.
+
+ discriminant = b ** 2 - 4 * a * c
+ 
+ #9.
+ 
+ c = math.sqrt(a ** 2 + b ** 2)
+ 
+ #10
+ 
+ b = math.sqrt(c ** 2 - a ** 2)
